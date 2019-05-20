ee98c29dd537b928fc71dec597319f129f07d079
DirtySneaker <edwinedwinedwin100@gmail.com>
2019-05-20 08:34:54 -0700

    CSS Course Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,7 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup"> 
        <h1>Hi people from Mexico</h1>
-       <p>Welcome to your new & Awesome Gatsby site. Welcome to your new & Awesome Gatsby site. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-       <p>Now go build something great & check it out.</p>
+       <p>Welcome to your new & Awesome Gatsby site. Welcome to your new & Awesome Gatsby site. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.</p>
         <Link to="/page-2/">Go to page 2</Link>
       </div>
     </div>
