e656f681352eeb22741eb41bb0db54e9fee7ca9a
DirtySneaker <edwinedwinedwin100@gmail.com>
2019-05-19 17:56:50 -0700

    Deleted all CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people from Mexico</h1>
     <p>Welcome to your new & Awesome Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great & check it out.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
