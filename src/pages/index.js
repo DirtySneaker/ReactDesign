a1aaca650144214c48a8d380bfe3b46c98e9b992
DirtySneaker <edwinedwinedwin100@gmail.com>
2019-05-22 07:55:24 -0700

    styled button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,11 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup"> 
-       <h1>Hi people from Mexico</h1>
+       <h1>Hi people from Mars</h1>
        <p>Welcome to your new & Awesome Gatsby site. Welcome to your new & Awesome Gatsby site. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <div className="ButtonStyle1">
+         <Link to="/page-2/">Go to page 2</Link>
+        </div>
       </div>
     </div>
   </div>
