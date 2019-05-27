029b9f1d51a24a56a73805ee048108fb26e49662
DirtySneaker <edwinedwinedwin100@gmail.com>
2019-05-27 09:45:40 -0700

    css animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup"> 
-       <h1>Hi people from Mars</h1>
+       <h1>Hi People From Mars</h1>
        <p>Welcome to your new & Awesome Gatsby site. Welcome to your new & Awesome Gatsby site. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.</p>
         <div className="ButtonStyle1">
          <Link to="/page-2/">Go to page 2</Link>
