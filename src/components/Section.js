788fc5d843449b8abdb6cd4b9d662862924390b4
DirtySneaker <edwinedwinedwin100@gmail.com>
2019-05-29 23:20:23 -0700

    finished section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,26 +1,71 @@
 import React from 'react'
-import styled from 'styledComponents'
+import styled from 'styled-Components'
+import Wave from './Wave';
 
 const SectionGroup = styled.div`
-    background: #000;
+    background: url(${props => props.image});
     height: 750px;
+    background-size: cover;
+    display: grid;
+    grid-template-rows: 300px auto;
+    grid-gap: 20px;
+    position: relative;
+
+    @media (max-width:640px) {
+        height: 820px;
+    }
 `
 const SectionLogo = styled.img `
+    align-self: end;
+    width: 120px;
+    margin: 0 auto;
 `
-const SectionTiteGroup = styled.div`
+const SectionTitleGroup = styled.div`
+    display: grid;
+    grid-template-columns: 300px auto;
+    grid-template-rows: auto 100%;
+    margin: 0 40px;
+    grid-gap: 20px;
+
+    @media (max-width: 720px) {
+        grid-template-columns: 1fr;
+    }
 `
 const SectionTitle = styled.h3`
+    color: #FFF;
+    font-size: 50px;
+    margin: 0;
+    line-height: 1.2;
+
+    @media (max-width: 720px) {
+        font-size: 32px;
+    }
 `
 const SectionText = styled.p`
+    color: #FFF;
+`
+const WaveBottom = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: -6px;
+`
+const WaveTop = styled.div`
+    position: absolute;
+    width: 100%;
+    top: -6px;
+    transform: rotate(180deg);
 `
 
 const Section = props => (
     <SectionGroup image={props.image}>
+        <WaveTop><Wave /></WaveTop>
+        <WaveBottom><Wave /></WaveBottom>
         <SectionLogo src={props.logo} />
-        <SectionTiteGroup>
+        <SectionTitleGroup>
             <SectionTitle>{props.title}</SectionTitle>
             <SectionText>{props.text}</SectionText>
-        </SectionTiteGroup>
+        </SectionTitleGroup>
+
     </SectionGroup>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Card from '../components/card';
 import Section from '../components/Section';
+import Wave from '../components/Wave';
 
 const IndexPage = () => (
   <div>
@@ -11,27 +12,16 @@ const IndexPage = () => (
        <p>Welcome to your new & Awesome Gatsby site. Welcome to your new & Awesome Gatsby site. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.</p>
         <div className="ButtonStyle1">
          <Link to="/page-2/">Go to page 2</Link>
-        <div className="Logos">
-          <img href="#" className="ToolsLogo" src={require('..//images/logo-sketch.png')} width="50px;" />
-          <img href="#" className="ToolsLogo" src={require('..//images/logo-figma.png')} width="50px;" />
-          <img href="#" className="ToolsLogo" src={require('..//images/logo-framer.png')} width="50px;" />
-          <img href="#" className="ToolsLogo" src={require('..//images/logo-studio.png')} width="50px;" />
-          <img href="#" className="ToolsLogo" src={require('..//images/logo-react.png')} width="50px;" />
-          <img href="#" className="ToolsLogo" src={require('..//images/logo-swift.png')} width="50px;" />
+          <div className="Logos">
+            <img href="#" className="ToolsLogo" src={require('..//images/logo-sketch.png')} width="50px;" />
+            <img href="#" className="ToolsLogo" src={require('..//images/logo-figma.png')} width="50px;" />
+            <img href="#" className="ToolsLogo" src={require('..//images/logo-framer.png')} width="50px;" />
+            <img href="#" className="ToolsLogo" src={require('..//images/logo-studio.png')} width="50px;" />
+            <img href="#" className="ToolsLogo" src={require('..//images/logo-react.png')} width="50px;" />
+            <img href="#" className="ToolsLogo" src={require('..//images/logo-swift.png')} width="50px;" />
+          </div>
         </div>
-         <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path 
-            fill="white">
-              <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-              M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
-              M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-              M0 36.6584C197 -31.5 394.5 54.5 544.5 54.5C694.5 54.5 855.555 0 1154 0C1440 0 1662 61 2560 36.6585V182.66L0 182.66V36.6584Z;
-              M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
-              "/>
-          </path>
-        </svg>
-
-        </div>
+        <Wave />
       </div>
     </div>
     <div className="Cards">
@@ -58,7 +48,7 @@ const IndexPage = () => (
     <Section 
     image={require('../images/wallpaper2.jpg')}
     logo={require('../images/logo-react.png')}
-    Title="React For Designers"
+    title="React For Designers"
     text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr."
     />
   </div>
